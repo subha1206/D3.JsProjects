aa2f70a5dcc023d3bcb72f1ea6ab84ae940b55e9
Subha Sarkar <sarkarsubha1206@gmail.com>
2020-05-14 16:56:39 +0530

    added json

--- basicsD3.js/app.js
+++ basicsD3.js/app.js
@@ -1,1 +1,26 @@
-const canvas = d3.select(".container")
+const canvas = d3.select(".container");
+
+const svg = canvas.append("svg").attr("width", 1000).attr("height", 600);
+
+const g = svg.append("g").attr("transform", "translate(220,100)");
+
+g.append("rect")
+  .attr("x", 100)
+  .attr("y", 100)
+  .attr("width", 200)
+  .attr("height", 200)
+  .attr("fill", "red");
+g.append("line")
+  .attr("x1", 40)
+  .attr("x2", 260)
+  .attr("y1", 100)
+  .attr("y2", 200)
+  .attr("stroke", "blue")
+  .attr("stroke-width", 3);
+g.append("text")
+  .attr("x", 500)
+  .attr("y", 350)
+  .attr("fill", "green")
+  .text("hello there")
+  .style("font-family", "arial")
+  .style("font-size", 50);
---
+++ dataWithD3.js/app.js
@@ -0,0 +1,49 @@
+// const data = [
+//   { width: 30, height: 450, fill: "pink" },
+//   { width: 50, height: 450, fill: "red" },
+//   { width: 50, height: 450, fill: "green" },
+// ];
+
+// const svg = d3.select("svg");
+
+// const rects = svg
+//   .selectAll("rect")
+//   .data(data)
+//   .attr("width", (d) => d.width)
+//   .attr("height", (d) => d.height)
+//   .attr("fill", (d) => d.fill)
+//   .attr("x", (d, i) => i * 50)
+//   .attr("y", 20);
+
+// rects
+//   .enter()
+//   .append("rect")
+//   .attr("width", (d) => d.width)
+//   .attr("height", (d) => d.height)
+//   .attr("fill", (d) => d.fill)
+//   .attr("x", (d, i) => i * 100)
+//   .attr("y", 20);
+
+const svg = d3.select("svg");
+
+d3.json("./planet.json").then((data) => {
+  const circles = svg.selectAll("circle").data(data);
+
+  // add attrr for existing(good practice)
+
+  circles
+    .attr("cy", 200)
+    .attr("cx", (d) => d.dis)
+    .attr("r", (d) => d.rad)
+    .attr("fill", (d) => d.fill);
+
+  // create new circlesusing enter selection
+
+  circles
+    .enter()
+    .append("circle")
+    .attr("cy", 200)
+    .attr("cx", (d) => d.dis)
+    .attr("r", (d) => d.rad)
+    .attr("fill", (d) => d.fill);
+});
